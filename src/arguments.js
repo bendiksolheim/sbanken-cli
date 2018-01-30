ecdec6293a00aef28f230b86b0b094f62386e38f
Bendik Solheim <bendik@solheim.im>
2018-01-30 23:20:41 +0100

    Module for parsing command line arguments

---
+++ src/arguments.js
@@ -0,0 +1,26 @@
+function help() {
+  console.log(`
+  Usage
+    $ sb <command>
+
+  where <command> is one of:
+    help             Display help text
+    balance          Display balance for all accounts
+  `);
+}
+
+function parse(options, args) {
+  const command = args[0];
+  if (!command || !options[command]) {
+    help();
+  } else {
+    options[command].apply(null, args.slice(1));
+  }
+}
+
+function create(options) {
+  options.help = help;
+  return parse.bind(null, options);
+}
+
+module.exports = { create };
