b47646b88701607998b58f5655642925786fbf48
Bendik Solheim <bendik@solheim.im>
2018-02-09 07:41:39 +0100

    Functions for fetching account info

--- src/index.js
+++ src/index.js
@@ -32,6 +32,18 @@ async function transactions(config, account) {
   }
 }
 
+async function accountInfo(config, account) {
+  try {
+    const accessToken = await api.accessToken(config.clientId, config.password);
+    const accounts = await api.accounts(accessToken, config.customerId);
+    const accountInfo = await api.accountInfo(accessToken, config.customerId, accounts.items, account);
+    print.printAccountInfo(accountInfo);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
 const config = conf.load();
 if (!config) {
   console.error('Could not find config file (.sbconfig) in home folder');
@@ -40,7 +52,8 @@ if (!config) {
 
 const parse = args.create({
   balance: () => balance(config),
-  transactions: account => transactions(config, account)
+  transactions: account => transactions(config, account),
+  account: account => accountInfo(config, account)
 });
 const programArguments = process.argv.slice(2);
 parse(programArguments);
