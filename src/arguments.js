a5ae0690746758e551af5ec6777ab7ad593fccc3
Bendik Solheim <bendik@solheim.im>
2018-02-13 07:45:32 +0100

    Add note about `sb transfer` in help text

--- src/arguments.js
+++ src/arguments.js
@@ -7,6 +7,7 @@ const helpText = `
     balance          Display balance for all accounts
     help             Display help text
     transactions     Display transactions for an account
+    transfer         Transfer between accounts
 
   sb help <command>  Show information about a specific command
 `;
